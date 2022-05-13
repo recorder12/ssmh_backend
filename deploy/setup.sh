75cedefc926590e2e887a57433522fcf6b51a977
Byeonggeon Kim <spinkbg@gmail.com>
2022-05-13 17:13:15 -0400

    edit deploy script

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -12,7 +12,7 @@ apt-get update
 apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
 
-rm -rf $PROJECT_BASE_PATH
+# rm -rf $PROJECT_BASE_PATH
 
 # Create project directory
 mkdir -p $PROJECT_BASE_PATH
