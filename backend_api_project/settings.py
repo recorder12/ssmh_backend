d278827a3dc91e6f265805d74ba69beb004ef700
Byeonggeon Kim <spinkbg@gmail.com>
2022-05-14 20:40:33 -0400

    remove cors app to avoid version conflict

--- backend_api_project/settings.py
+++ backend_api_project/settings.py
@@ -40,12 +40,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'profiles_api',
-    'corsheaders',
+    # 'corsheaders',
 ]
 
 
 MIDDLEWARE_CLASSES = (
-    #...
     'app.CorsMiddleware'
 )
 
@@ -54,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -147,7 +146,7 @@ AUTH_USER_MODEL = 'profiles_api.UserProfile'
 
 STATIC_ROOT = 'static/'
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
 
 # CORS_ALLOWED_ORIGINS = ['*']
